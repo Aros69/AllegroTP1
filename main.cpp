94175012de2530f54065ba58236e8a5355c5fd42
aros69 <donnay.robin@gmail.com>
2024-11-25 11:37:45 +0100

    Inital Commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <cstdint>
+#include <iostream>
+#include <allegro5/allegro.h>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+
+    if(!al_init())
+        return -1;
+
+    ALLEGRO_DISPLAY* window = al_create_display(1280, 720);
+    al_set_window_title(window, "HelloAllegro");
+
+    while(true)
+    {}
+
+    al_destroy_display(window);
+
+    return 0;
+}
+
+/*
+ *
+ */
